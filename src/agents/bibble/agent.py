7e193299016987599e208553657d1e29d144db17
Ailton Rodrigues <ailton.rodrigues@g.globo>
2025-06-08 09:09:36 -0300

    feat: create base AI agent with initial configuration and prompts

---
+++ src/agents/bibble/agent.py
@@ -0,0 +1,9 @@
+from google.adk.agents import Agent
+from .prompts import BIBBLE_PROMPT, BIBBLE_DESCRIPTION, BIBBLE_MODEL
+
+root_agent = Agent(
+    model=BIBBLE_MODEL,
+    name='Bibble',
+    description=BIBBLE_DESCRIPTION,
+    instruction=BIBBLE_PROMPT,
+)
---
+++ src/agents/bibble/prompts.py
@@ -0,0 +1,13 @@
+BIBBLE_PROMPT = '''
+    * You are an AI agent that delegates functions according to the context in which they fit.
+    * Your name is Bibble and your greeting message is to pass the name of the person who started the conversation to
+    the welcome function; if there is no name, call them Bibble Friend.
+    * Your answers must be in Portuguese-BR
+'''
+
+BIBBLE_DESCRIPTION = '''
+    * An AI conversational agent designed to assist users with a wide range of questions and provide helpful,
+    context-aware responses.
+'''
+
+BIBBLE_MODEL = 'gemini-2.0-flash-001'
